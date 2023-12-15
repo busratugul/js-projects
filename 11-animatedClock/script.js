639ccad0568db78523ee63d36169834cf7f6806b
Büşra Tuğul <134439412+busratugul@users.noreply.github.com>
2023-12-15 22:36:13 +0300

    animatedClock
    Saatin parçalarını istediğimi renge çevirebileceğimiz bir form ekledim.

--- 11-animatedClock/script.js
+++ 11-animatedClock/script.js
@@ -1,3 +1,9 @@
+const faceColor = document.getElementById('face-color')
+const borderColor = document.getElementById('border-color')
+const lineColor = document.getElementById('line-color')
+const largeHandColor = document.getElementById('large-hand-color')
+const secondHandColor = document.getElementById('second-hand-color')
+
 function clock() {
   const now = new Date()
   const canvas = document.getElementById('canvas')
@@ -19,7 +25,8 @@ function clock() {
   ctx.save()
   ctx.beginPath()
   ctx.lineWidth = 14
-  ctx.strokeStyle = '#800000'
+  ctx.strokeStyle = borderColor.value
+  ctx.fillStyle = faceColor.value
   ctx.arc(0, 0, 142, 0, Math.PI * 2, true)
   ctx.stroke()
   ctx.fill()
@@ -27,6 +34,7 @@ function clock() {
 
   //Draw hour lines
   ctx.save()
+  ctx.strokeStyle = lineColor.value
   for (let i = 0; i < 12; i++) {
     ctx.beginPath()
     ctx.rotate(Math.PI / 6)
@@ -39,6 +47,7 @@ function clock() {
   //Draw minute lines
   ctx.save()
   ctx.lineWidth = 4
+  ctx.strokeStyle = lineColor.value
   for (let i = 0; i < 60; i++) {
     if (i % 5 !== 0) {
       ctx.beginPath()
@@ -61,7 +70,7 @@ function clock() {
   ctx.rotate(
     (Math.PI / 6) * hr + (Math.PI / 360) * min + (Math.PI / 21600) * sec
   )
-  ctx.strokeStyle = '#800000'
+  ctx.strokeStyle = largeHandColor.value
   ctx.lineWidth = 14
   ctx.beginPath()
   ctx.moveTo(-20, 0)
@@ -72,7 +81,7 @@ function clock() {
   //Draw min hand
   ctx.save()
   ctx.rotate((Math.PI / 30) * min + (Math.PI / 1800) * sec)
-  ctx.strokeStyle = '#800000'
+  ctx.strokeStyle = largeHandColor.value
   ctx.lineWidth = 10
   ctx.beginPath()
   ctx.moveTo(-29, 0)
@@ -80,11 +89,11 @@ function clock() {
   ctx.stroke()
   ctx.restore()
 
-  //Draw SEC hand
+  //Draw sec hand
   ctx.save()
   ctx.rotate((sec * Math.PI) / 30)
-  ctx.strokeStyle = '#ff7f50'
-  ctx.fillStyle = '#ff7f50'
+  ctx.strokeStyle = secondHandColor.value
+  ctx.fillStyle = secondHandColor.value
   ctx.lineWidth = 6
   ctx.beginPath()
   ctx.moveTo(-30, 0)
@@ -96,9 +105,15 @@ function clock() {
   ctx.restore()
 
   ctx.restore() //restore default state
-  requestAnimationFrame(clock);
-
+  requestAnimationFrame(clock)
 }
 
+requestAnimationFrame(clock)
 
-requestAnimationFrame(clock);
+document.getElementById('save-btn').addEventListener('click', () => {
+  const dataURL = canvas.toDataURL('image/png')
+  const link = document.createElement('a')
+  link.download = 'clock.png'
+  link.href = dataURL
+  link.click()
+})
