58569a39fe012b11c6092cc4945395e3b7295caf
Busra Tugul <bsratugul@gmail.com>
2024-02-28 16:01:57 +0300

    pagination tamamlandı.

--- 16-FlixMovie/js/script.js
+++ 16-FlixMovie/js/script.js
@@ -5,6 +5,7 @@ const global = {
     type: '',
     page: 1,
     totalPages: 1,
+    totalResults: 0,
   },
   api: {
     API_KEY: 'd00d9961e58ed9712fc47c1324422299',
@@ -251,7 +252,12 @@ async function search() {
   global.search.term = urlParams.get('search-term')
 
   if (global.search.term !== '' && global.search.term !== null) {
-    const { results, total_pages, page } = await searchAPIData()
+    const { results, total_pages, page, total_results } = await searchAPIData()
+
+    global.search.page = page
+    global.search.totalPages = total_pages
+    global.search.totalResults = total_results
+
     if (results.length === 0) {
       showAlert('No results found')
       return
@@ -264,6 +270,11 @@ async function search() {
 }
 
 function displaySearchResults(results) {
+  //clear previous results
+  document.querySelector('#search-results').innerHTML = ''
+  document.querySelector('#search-results-heading').innerHTML = ''
+  document.querySelector('#pagination').innerHTML = ''
+
   results.forEach((result) => {
     const div = document.createElement('div')
     div.classList.add('card')
@@ -291,18 +302,60 @@ function displaySearchResults(results) {
              }
             </a>
             <div class="card-body">
-              <h5 class="card-title">${global.search.type === 'movie' ? result.title : result.name}</h5>
+              <h5 class="card-title">${
+                global.search.type === 'movie' ? result.title : result.name
+              }</h5>
               <p class="card-text">
                 <small class="text-muted">Release: ${
                   global.search.type === 'movie'
                     ? result.release_date
                     : result.first_air_date
                 }
-                }</small>
+                </small>
               </p>
             </div>
           `
+    document.querySelector(
+      '#search-results-heading'
+    ).innerHTML = `<h2>${results.length} of ${global.search.totalResults} Results for ${global.search.term}</h2>`
     document.querySelector('#search-results').appendChild(div)
+  })
+  displayPagination()
+}
+
+//create & display pagination for search
+function displayPagination() {
+  const div = document.createElement('div')
+  div.classList.add('pagination')
+  div.innerHTML = `
+  <button class="btn btn-primary" id="prev">Prev</button>
+          <button class="btn btn-primary" id="next">Next</button>
+          <div class="page-counter">Page ${global.search.page} of ${global.search.totalPages}</div>
+  `
+  document.querySelector('#pagination').appendChild(div)
+
+  //Disable prev button if on first page
+  if (global.search.page === 1) {
+    document.querySelector('#prev').disabled = true
+  }
+
+  //Disable next button if on last page
+  if (global.search.page === global.search.totalPages) {
+    document.querySelector('#next').disabled = true
+  }
+
+  //Next Page
+  document.querySelector('#next').addEventListener('click', async () => {
+    global.search.page++
+    const { results, total_pages } = await searchAPIData()
+    displaySearchResults(results)
+  })
+
+  //Prev Page
+  document.querySelector('#prev').addEventListener('click', async () => {
+    global.search.page--
+    const { results, total_pages } = await searchAPIData()
+    displaySearchResults(results)
   })
 }
 
@@ -363,7 +416,7 @@ async function fetchAPIData(endpoint) {
   const data = await response.json()
 
   hideSpinner()
-
+  ƒ
   return data
 }
 
@@ -375,7 +428,7 @@ async function searchAPIData() {
   showSpinner()
 
   const response = await fetch(
-    `${API_URL}search/${global.search.type}?api_key=${API_KEY}&language=en-US&query=${global.search.term}`
+    `${API_URL}search/${global.search.type}?api_key=${API_KEY}&language=en-US&query=${global.search.term}&page=${global.search.page}`
   )
   const data = await response.json()
 
